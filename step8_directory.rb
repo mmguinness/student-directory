2e5780b86b9622d64f6708b036225d3d18b799cd
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-17 16:40:16 +0000

    Step 8 exercise 7 added, questions with propmts added.

--- step8_directory.rb
+++ step8_directory.rb
@@ -7,10 +7,20 @@ def input_students
   name = gets.chomp
   # while the name is not empty repeat this code
   while !name.empty? do
-    #add the student hash to the array
+    puts "Cohort".center(65, '||  ||')
+    cohort = gets.chomp
+      if cohort != "Jan 2022"
+      puts "Did you mean January 2022?"
+      answer = gets.chomp
+        if answer == "\n" then cohort = "Jan 2022" end
+        if answer == "yes" then cohort = "Jan 2022" end
+      end
     puts "Fav hobby".center(65, ' <   > ')
     hobby = gets.chomp
-    students << {name: name, cohort: :november, hobby: hobby}
+    puts "Press enter if #{hobby} is correct, otherwise enter fav hobby"
+    answer = gets.chomp
+     if answer == '\n' then hobby = answer end
+    students << {name: name, cohort: cohort, hobby: hobby}
     puts "Now we have #{students.count} students".center(65)
     # get annother name from the user
     name = gets.chomp
@@ -26,7 +36,7 @@ end
 
 def print(students)
   while students.size >= 1
-    puts "#{students[0][:name]} likes #{students[0][:hobby]}".center(65, ' * ')
+    puts "#{students[0][:name]} from #{students[0][:cohort]} cohort likes #{students[0][:hobby]}".center(65, ' * ')
     students.shift
   end
 end
