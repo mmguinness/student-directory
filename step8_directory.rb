56576c329094b9ee70be2834582c4f442d95d918
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-17 18:16:43 +0000

    Step 8 exercise 8, students grouped by cohort.

--- step8_directory.rb
+++ step8_directory.rb
@@ -1,5 +1,5 @@
 def input_students
-  puts "Please enter the names of the students, followed by their hobbies"
+  puts "Please enter the names of the students, followed by their cohort"
   puts "To finish, just hit return twice".center(65)
   # create and empty array
   students = []
@@ -7,7 +7,7 @@ def input_students
   name = gets.chomp
   # while the name is not empty repeat this code
   while !name.empty? do
-    puts "Cohort".center(65, '||  ||')
+    puts "Cohort".center(65)
     cohort = gets.chomp
       if cohort != "Jan 2022"
       puts "Did you mean January 2022?"
@@ -15,12 +15,7 @@ def input_students
         if answer == "\n" then cohort = "Jan 2022" end
         if answer == "yes" then cohort = "Jan 2022" end
       end
-    puts "Fav hobby".center(65, ' <   > ')
-    hobby = gets.chomp
-    puts "Press enter if #{hobby} is correct, otherwise enter fav hobby"
-    answer = gets.chomp
-     if answer == '\n' then hobby = answer end
-    students << {name: name, cohort: cohort, hobby: hobby}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students".center(65)
     # get annother name from the user
     name = gets.chomp
@@ -35,9 +30,22 @@ def print_header
 end
 
 def print(students)
-  while students.size >= 1
-    puts "#{students[0][:name]} from #{students[0][:cohort]} cohort likes #{students[0][:hobby]}".center(65, ' * ')
-    students.shift
+  cohort_options = []
+  students.select do |student|
+    cohort_options << student[:cohort]
+  end
+  cohort_options.uniq!
+  puts "The students by cohort:".center(65)
+
+  
+  while cohort_options.size >= 1
+    puts cohort_options[0].upcase.center(65, " * ")
+    students.select do |student|
+      if student[:cohort] == cohort_options[0]
+      puts "#{student[:name]}".center(65)
+      end
+    end
+    cohort_options.shift
   end
 end
 
@@ -46,6 +54,4 @@ def print_footer(students)
 end
 
 students = input_students
-print_header
-print_footer(students)
 print(students)
