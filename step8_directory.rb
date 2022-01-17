2ac9d7c09a28d4a3d059fbb1a1fd85df78d0b1c5
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-17 16:17:46 +0000

    Step 8 exercise 6, new alignment added to text.

--- step8_directory.rb
+++ step8_directory.rb
@@ -1,6 +1,6 @@
 def input_students
   puts "Please enter the names of the students, followed by their hobbies"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return twice".center(65)
   # create and empty array
   students = []
   # get the first name
@@ -8,10 +8,10 @@ def input_students
   # while the name is not empty repeat this code
   while !name.empty? do
     #add the student hash to the array
-    puts "Fav hobby"
+    puts "Fav hobby".center(65, ' <   > ')
     hobby = gets.chomp
     students << {name: name, cohort: :november, hobby: hobby}
-    puts "Now we have #{students.count} students"
+    puts "Now we have #{students.count} students".center(65)
     # get annother name from the user
     name = gets.chomp
   end
@@ -20,22 +20,22 @@ def input_students
 end
 
 def print_header
-  puts "The students of my cohort at Makers"
-  puts "------------"
+  puts "The students of my cohort at Makers".center(65)
+  puts "------------".center(65, '-')
 end
 
 def print(students)
   while students.size >= 1
-    puts "#{students[0][:name]} likes #{students[0][:hobby]}"
+    puts "#{students[0][:name]} likes #{students[0][:hobby]}".center(65, ' * ')
     students.shift
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.size} great students".center(65)
 end
 
 students = input_students
 print_header
-print(students)
 print_footer(students)
+print(students)
