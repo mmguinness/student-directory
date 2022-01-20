2acd32a7d3f19cac7779da3327a42e952bfc9a43
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-01-20 12:08:13 +0000

    step 14, exercise 4, confirmation of input included in program.

--- step14_directory.rb
+++ step14_directory.rb
@@ -81,13 +81,28 @@ end
 
 def process(selection)
   case selection
-  when "1", "1." then input_students
-  when "2", "2." then show_students
-  when "3", "3." then save_students
-  when "4", "4." then load_students
-  when "9", "9." then exit
-  else puts " I don't know what you meant, try again"
+  when "1", "1." 
+    confirmation
+    input_students
+  when "2", "2."
+    confirmation
+    show_students
+  when "3", "3."
+    confirmation
+    save_students
+  when "4", "4."
+    confirmation
+    load_students
+  when "9", "9."
+    confirmation
+    exit
+  else 
+    puts " I don't know what you meant, try again"
   end
+end
+
+def confirmation
+  puts "Input accepted."
 end
 
 def interactive_menu
